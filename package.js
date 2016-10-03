2d1ea248d6f09a1dca718968d0763c057243eb47
xavcz <xavier.cazalot@gmail.com>
2016-10-03 17:08:45 +0200

    v0.27.2

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "xavcz:nova-awesome-vote",
   summary: "Nova component vote extension pacakge",
-  version: "0.27.1-nova",
+  version: "0.27.2-nova",
   git: "https://github.com/xavcz/nova-awesome-vote.git"
 });
 
